69dbe755fbe2c77746e2df13519eb87c11c151bb
Leonid Markov <mypochta75@gmail.com>
2018-04-26 19:00:46 +0300

    small changes

---
+++ js/main.js
@@ -0,0 +1,50 @@
+$(function() {
+$('.carousel').carousel({
+      interval: 20000;
+    });
+
+
+/*$('.slider').slick({
+  infinite: true,
+  slidesToShow: 2,
+  slidesToScroll: 2,  
+  dots: true,
+  speed: 1000,  
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 960,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    ]  
+  
+});*/
+
+});
