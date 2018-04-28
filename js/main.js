c83fdd36f4c8764d45d595565c46712388d392ce
Leonid Markov <mypochta75@gmail.com>
2018-04-28 18:46:27 +0300

    Complete refactoring

--- js/main.js
+++ js/main.js
@@ -5,7 +5,9 @@ $('.slider').slick({
   slidesToScroll: 1,  
   dots: true,
   dotsClass: 'slick-dots',
-  speed: 1000  
+  speed: 1000,
+  prevArrow: '<img class="prev" src="img/slide-left.png" alt="prev" />',
+  nextArrow: '<img class="next" src="img/slide-right.png" alt="next" />' 
   /*responsive: [
     {
       breakpoint: 1024,
