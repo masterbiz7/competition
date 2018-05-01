dc388d3249d1e5822fcc7e21394427bcebc9446b
Leonid Markov <mypochta75@gmail.com>
2018-05-01 19:25:02 +0300

    Сделан адаптив на малые экраны

--- js/main.js
+++ js/main.js
@@ -8,43 +8,6 @@ $('.slider').slick({
   speed: 1000,
   prevArrow: '<img class="prev" src="img/slide-left.png" alt="prev" />',
   nextArrow: '<img class="next" src="img/slide-right.png" alt="next" />' 
-  /*responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true,
-        prevArrow: '<img class="prev" src="img/slide-left.png" alt="prev" />',
-        nextArrow: '<img class="next" src="img/slide-right.png" alt="next" />'
-      }
-    },
-    {
-      breakpoint: 960,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    ] */
-  
+ 
 });
-
 });
---
+++ js/menu.js
@@ -0,0 +1,23 @@
+$(function() {
+	 if ($(window).width() == 560) {
+	 	$(".fixed-menu").css("top", "-1000px");
+	 } else {
+	 	$(".fixed-menu").css("top", "0");
+	 }
+
+	var flag = 0;
+	$(".hamburger-menu").click(function() {
+		if(flag==0) {
+		$(".hidden-menu__items").css("top", "70px");
+		flag = 1;
+	} else {
+		$(".hidden-menu__items").css("top", "-5000px");
+		flag = 0;
+	}
+	});
+});
+
+
+/*
+(!window.matchMedia('(min-width: 425px)').matches)
+*/
